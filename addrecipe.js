71f0e11687f703541b966f34d5934a31381a9feb
Nathan Carter <97918203+nathandcarter21@users.noreply.github.com>
2023-03-03 01:26:10 -0700

    Add recipes to local storage

---
+++ addrecipe.js
@@ -0,0 +1,22 @@
+const addRecipe = () => {
+    const name = document.querySelector('#name').value
+    const ingredients = document.querySelector('#ingredients').value.split(',')
+    const servings = document.querySelector('#servings').value
+    const calories = document.querySelector('#calories').value
+    const type = document.querySelector('#type').value
+    const price = document.querySelector('#price').value
+
+    const recipe = {
+        name, ingredients, servings, calories, type, price
+    }
+
+    let recipes = JSON.parse(localStorage.getItem('recipes'))
+
+    if (recipes === null)
+        recipes = [recipe]
+    else
+        recipes.push(recipe)
+
+    localStorage.setItem('recipes', JSON.stringify(recipes))
+    window.location.href = 'myrecipes.html'
+}
