b259dbe80d4abe9ac1f96f0cca007e5ccf149f46
Nathan Carter <97918203+nathandcarter21@users.noreply.github.com>
2023-03-03 01:25:30 -0700

    Dynamically render recipes

--- app.js
+++ app.js
@@ -1,3 +1,47 @@
 function showDropDown() {
     document.getElementById("dropdownNav").classList.toggle("show");
 }
+
+const logout = () => {
+    localStorage.setItem('username', '')
+    localStorage.setItem('recipes', null)
+    window.location.href = 'index.html'
+    const account = document.querySelector('.account')
+    account.innerText = 'Account'
+
+    const dropdownNav = document.querySelector('.dropdownNav')
+
+    const login = document.createElement('a')
+    login.innerText = 'Login'
+    login.href = 'login.html'
+    login.className += 'block'
+
+    dropdownNav.replaceChildren(login)
+}
+
+const setUsername = () => {
+    const username = localStorage.getItem('username')
+    const account = document.querySelector('.account')
+    if (username === "") {
+        return
+    }
+    account.innerText = username
+
+    const dropdownNav = document.querySelector('.dropdownNav')
+
+    const myRecipes = document.createElement('a')
+    myRecipes.innerText = 'My Recipes'
+    myRecipes.href = 'myrecipes.html'
+    myRecipes.className += 'block'
+
+    const logoutBtn = document.createElement('a')
+    logoutBtn.innerText = 'Log Out'
+    logoutBtn.href = '#'
+    logoutBtn.className += 'block'
+    logoutBtn.addEventListener('click', logout)
+
+    dropdownNav.replaceChildren(myRecipes, logoutBtn)
+}
+
+
+setUsername()
